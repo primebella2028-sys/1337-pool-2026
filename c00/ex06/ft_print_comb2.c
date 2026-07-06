b9418f7dbe97ddd717ccf142f482f3b60ea4fe96
Ayoub bella <prime.bella.2028@gmail.com>
2026-07-06 13:01:07 +0100

    c00

---
+++ c00/ex06/ft_print_comb2.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Aybella <prime.bella.2028@student.42.fr>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/06 12:43:10 by Aybella           #+#    #+#             */
+/*   Updated: 2026/07/06 13:00:48 by Aybella          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_comb2(void)
+{
+	int	a;
+	int	b;
+
+	a = 0;
+	while (a <= 98)
+	{
+		b = a + 1;
+		while (b <= 99)
+		{
+			ft_putchar((a / 10) + '0');
+			ft_putchar((a % 10) + '0');
+			ft_putchar(' ');
+			ft_putchar((b / 10) + '0');
+			ft_putchar((b % 10) + '0');
+			if (!(a == 98 && b == 99))
+				write(1, ", ", 2);
+			b++;
+		}
+		a++;
+	}
+	write(1, "\n", 1);
+}
+
+int	main(void)
+{
+	ft_print_comb2();
+	return (0);
+}
