508617ca98b48f2f716da4bc5c8ca6dbcc999da6
Ayoub bella <prime.bella.2028@gmail.com>
2026-07-06 11:15:13 +0100

    c00

--- c00/ex02/ft_print_reverse_alphabet.c
+++ c00/ex02/ft_print_reverse_alphabet.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: Aybella <prime.bella.2028@student.42.fr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/07/06 11:05:15 by Aybella           #+#    #+#             */
-/*   Updated: 2026/07/06 11:14:43 by Aybella          ###   ########.fr       */
+/*   Created: 2026/07/06 11:10:26 by Aybella           #+#    #+#             */
+/*   Updated: 2026/07/06 11:13:58 by Aybella          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
@@ -16,24 +16,22 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-void	ft_print_alphabet(void)
+void	ft_print_reverse_alphabet(void)
 {
-
 	char	c;
 
-	c = 'a';
-	while (c <= 'z')
+	c = 'z';
+	while (c >= 'a')
 	{
 		ft_putchar(c);
-		c++;
+		c--;
 	}
 }
 
 
-
-int main()
+int	main()
 {
-	ft_print_alphabet();
+	ft_print_reverse_alphabet();
 
 	return (0);
 }
